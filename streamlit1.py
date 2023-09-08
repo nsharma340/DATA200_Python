6bf88362c373c7c870b60071cb663ca6d974d0c8
Neha Sharma <76728522+nsharma340@users.noreply.github.com>
2023-09-08 15:09:41 -0700

    Add files via upload

---
+++ streamlit1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+import pandas as pd
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
