c42e2e220688d2b54f33bc8f84f5d8205572a0e7
Neha Sharma <76728522+nsharma340@users.noreply.github.com>
2023-09-14 20:29:31 -0700

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+toy = pd.read_csv('toy_dataset.csv')
+df = pd.DataFrame(toy)
+X = list(df.iloc[:, 2])
+Y = list(df.iloc[:, 4])
+#Plot the data using bar() method
+plt.bar(X, Y, color = 'b')
+plt.title("Gender vs illness variation")
+plt.xlabel("Gender")
+plt.ylabel("illness")
+
+#show the plot
+plt.show()
+
