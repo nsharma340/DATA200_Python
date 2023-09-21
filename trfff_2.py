d511771c412bc0996d520056c93a42a9539eabb4
Neha Sharma <76728522+nsharma340@users.noreply.github.com>
2023-09-21 06:29:50 -0700

    Add files via upload

---
+++ trfff_2.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+data = pd.read_csv("Netflix Userbase.csv")
+
+st.markdown("# Dataset Top 5")
+#st.write("Dataset Top 5")
+st.write(data.head())
+
+st.title("Type of Device used by people")
+attribute_counts = data['Device'].value_counts()
+st.bar_chart(attribute_counts)
+
+st.title("Type of Device used by people")
+country_counts = data['Country'].value_counts()
+st.line_chart(country_counts, color = ['#FF0000'])
+
+# Create a multiselect widget to select countries
+selected_countries = st.multiselect("Select countries", data['Country'])
+
+# Filter the DataFrame based on the selected countries
+filtered_data = data[data['Country'].isin(selected_countries)]
+
+# Display the filtered data
+st.write("Selected Data", filtered_data)
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+# Plot value counts in a bar chart
+if not filtered_data.empty:
+    st.write("Value Counts")
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=filtered_data['Country'].value_counts().index, y=filtered_data['Country'].value_counts().values, data=filtered_data)
+    #plt.xticks(rotation=45)
+    st.pyplot()
+
+
+if not filtered_data.empty:
+    st.markdown("## Device Counts by Country")
+    device_counts = filtered_data.groupby('Country')['Device'].value_counts().unstack(fill_value=0)
+    st.bar_chart(device_counts, use_container_width=True)
