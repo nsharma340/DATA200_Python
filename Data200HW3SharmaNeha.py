22792f4e183d5c4cc52d17f19adf51bdfafdd993
Neha Sharma <76728522+nsharma340@users.noreply.github.com>
2023-09-21 07:02:23 -0700

    Add files via upload

--- Data200HW3SharmaNeha.py
+++ Data200HW3SharmaNeha.py
@@ -8,15 +8,17 @@ st.markdown("# Dataset Top 5")
 #st.write("Dataset Top 5")
 st.write(data.head())
 
-st.title("Type of Device used by people")
+st.title("Type of Device used by Netflix Users")
 attribute_counts = data['Device'].value_counts()
 st.bar_chart(attribute_counts)
 
-st.title("Type of Device used by people")
+st.title("Netflix Users across Countries")
 country_counts = data['Country'].value_counts()
 st.line_chart(country_counts, color = ['#FF0000'])
 
+
 # Create a multiselect widget to select countries
+st.markdown("## Select Country")
 selected_countries = st.multiselect("Select countries", data['Country'])
 
 # Filter the DataFrame based on the selected countries
@@ -29,7 +31,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Plot value counts in a bar chart
 if not filtered_data.empty:
-    st.write("Value Counts")
+    st.write("# Number of Netflix users by country")
     sns.set(style="whitegrid")
     plt.figure(figsize=(10, 6))
     sns.barplot(x=filtered_data['Country'].value_counts().index, y=filtered_data['Country'].value_counts().values, data=filtered_data)
@@ -38,6 +40,6 @@ if not filtered_data.empty:
 
 
 if not filtered_data.empty:
-    st.markdown("## Device Counts by Country")
+    st.markdown("## Type of Device used by users filtered by Country")
     device_counts = filtered_data.groupby('Country')['Device'].value_counts().unstack(fill_value=0)
     st.bar_chart(device_counts, use_container_width=True)
