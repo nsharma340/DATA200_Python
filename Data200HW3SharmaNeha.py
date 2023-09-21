4efd164823fad0c7540e40a85992f471798e61db
Neha Sharma <76728522+nsharma340@users.noreply.github.com>
2023-09-21 10:07:55 -0700

    Add files via upload

--- Data200HW3SharmaNeha.py
+++ Data200HW3SharmaNeha.py
@@ -2,21 +2,46 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import altair as alt
+
 data = pd.read_csv("Netflix Userbase.csv")
 
 st.markdown("# Dataset Top 5")
 #st.write("Dataset Top 5")
 st.write(data.head())
 
+###2. using the altair charts
 st.title("Type of Device used by Netflix Users")
-attribute_counts = data['Device'].value_counts()
-st.bar_chart(attribute_counts)
+attribute_counts = data['Device'].value_counts().reset_index()
+attribute_counts.columns = ['Device', 'Count']
 
+c = alt.Chart(attribute_counts).mark_bar().encode(
+    x='Device:O',  # Use 'O' for ordinal data (text)
+    y='Count:Q',   # Use 'Q' for quantitative data (numeric)
+    tooltip=['Device', 'Count']  # Display tooltips on hover
+)
+
+st.altair_chart(c, use_container_width=True)
+
+
+
+###5. using the altair charts
 st.title("Netflix Users across Countries")
-country_counts = data['Country'].value_counts()
-st.line_chart(country_counts, color = ['#FF0000'])
+country_counts = data['Country'].value_counts().reset_index()
+country_counts.columns = ['Country', 'Count']
+
+c = alt.Chart(country_counts).mark_line().encode(
+    x='Country:O',  # Use 'O' for ordinal data (text)
+    y='Count:Q',   # Use 'Q' for quantitative data (numeric)
+    tooltip=['Country', 'Count']  # Display tooltips on hover
+    #color = ['#FF0000']
+)
+
+st.altair_chart(c, use_container_width=True)
 
 
+
+###7. Multiselect widget creation
 # Create a multiselect widget to select countries
 st.markdown("## Select Country")
 selected_countries = st.multiselect("Select countries", data['Country'])
@@ -43,3 +68,4 @@ if not filtered_data.empty:
     st.markdown("## Type of Device used by users filtered by Country")
     device_counts = filtered_data.groupby('Country')['Device'].value_counts().unstack(fill_value=0)
     st.bar_chart(device_counts, use_container_width=True)
+    
