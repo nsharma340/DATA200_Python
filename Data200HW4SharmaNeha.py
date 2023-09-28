cea5f4c9eb91b4af67ebf9df28715fc298dfae60
Neha Sharma <76728522+nsharma340@users.noreply.github.com>
2023-09-27 20:28:48 -0700

    Add files via upload

---
+++ Data200HW4SharmaNeha.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+
+data = pd.read_csv('retail_sales_dataset.csv')
+df = pd.DataFrame(data)
+st.title("product category where most sales happened")
+attribute_counts = data['Product Category'].value_counts().reset_index()
+attribute_counts.columns = ['Product Category','Count']
+
+c = alt.Chart(attribute_counts).mark_bar().encode(
+    x = 'Product Category:O',
+    y = 'Count:Q',
+    tooltip = ['Product Category', 'Count']
+)
+st.altair_chart(c, use_container_width = True)
+
+st.title("Shoppers according to Age")
+attribute_age = data['Age'].value_counts().reset_index()
+attribute_age.columns = ['Age','Count']
+
+d = alt.Chart(attribute_age).mark_bar().encode(
+    x = 'Age:O',
+    y = 'Count:Q',
+    tooltip = ['Age', 'Count']
+)
+st.altair_chart(d, use_container_width = True)
+
+
+ 
